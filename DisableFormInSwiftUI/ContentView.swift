1b01d8be3ec024b06b5532d7eb41758cc14d88ee
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-28 22:02:46 +0300

    Initial Commit

---
+++ DisableFormInSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DisableFormInSwiftUI
+//
+//  Created by Ramill Ibragimov on 28.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DisableFormInSwiftUI/DisableFormInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisableFormInSwiftUIApp.swift
+//  DisableFormInSwiftUI
+//
+//  Created by Ramill Ibragimov on 28.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct DisableFormInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
